1b6b1e45c7f31f0053b795fd9c3aa125f4c84f7e
Andoni Torres <71408357+Trufatrula@users.noreply.github.com>
2022-06-16 00:25:53 +0200

    Puntuacion dinamica de LOCURA

--- Assets/Scripts/HighScores.cs
+++ Assets/Scripts/HighScores.cs
@@ -4,20 +4,21 @@ public class HighScores : MonoBehaviour
 {
     public HighScoreDisplay[] highScoreDisplayArray;
     List<HighScoreEntry> scores = new List<HighScoreEntry>();
+    
     void Start()
     {
-
-        AddNewScore("Pichulon", 4500);
-        AddNewScore("Perro Sancho", 5520);
-        AddNewScore("Los huevos de pascua", 380);
-        AddNewScore("Del San Andreas", 6654);
-        AddNewScore("ASDFGHJ", 11021);
-        AddNewScore("tgg7", 3252);
+        AddNewScore("John", 4500);
+        AddNewScore("Max", 5520);
+        AddNewScore("Dave", 380);
+        AddNewScore("Steve", 6654);
+        AddNewScore("Mike", 11021);
+        AddNewScore("Teddy", 3252);
         UpdateDisplay();
+        XMLManager.instance.SaveScores(scores);    
     }
     void UpdateDisplay()
     {
-        scores.Sort((HighScoreEntry x, HighScoreEntry y) => y.score.CompareTo(x.score));
+        scores.Sort((HighScoreEntry x, HighScoreEntry y) => x.score.CompareTo(y.score));
         for (int i = 0; i < highScoreDisplayArray.Length; i++)
         {
             if (i < scores.Count)
@@ -30,6 +31,7 @@ public class HighScores : MonoBehaviour
             }
         }
     }
+    
     void AddNewScore(string entryName, int entryScore)
     {
         scores.Add(new HighScoreEntry { name = entryName, score = entryScore });
